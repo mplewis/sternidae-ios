2a8e6b239d22bd4e296ed2decc1d7e405f764b05
Matthew Lewis <matt@mplewis.com>
2016-09-17 14:26:09 -0600

    git: initial commit

---
+++ Sternidae/CompassView.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let pi = CGFloat(M_PI)
+
+@IBDesignable class CompassView: UIView {
+    
+    @IBInspectable var outlineColor: UIColor = .whiteColor()
+    @IBInspectable var thickness: CGFloat = 5
+    
+    override func drawRect(rect: CGRect) {
+        let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+        let radius = (min(bounds.width, bounds.height) - thickness) / 2
+        
+        outlineColor.setStroke()
+        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * pi, clockwise: true)
+        path.lineWidth = thickness
+        path.stroke()
+    }
+
+}
