b20ed5c5e8abd78b06735634159c1d3bdfaa3178
Matthew Lewis <matt@mplewis.com>
2016-09-19 21:02:11 -0600

    app: use location services

--- Sternidae/AppDelegate.swift
+++ Sternidae/AppDelegate.swift
@@ -1,16 +1,44 @@
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
-
+    var locMgr: CLLocationManager?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        startListeningForLocation()
         return true
     }
-
-
+    
+    func startListeningForLocation() {
+        locMgr = CLLocationManager()
+        locMgr?.delegate = self
+        let status = CLLocationManager.authorizationStatus()
+        if status == .NotDetermined {
+            locMgr?.requestWhenInUseAuthorization()
+        } else {
+            startMonitoring(status)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        startMonitoring(status)
+    }
+    
+    func startMonitoring(status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+            locMgr?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locMgr?.startUpdatingLocation()
+            print("Location services started")
+        } else {
+            print("Location permission denied")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        print("New location: \(newLocation)")
+    }
+    
 }
-
