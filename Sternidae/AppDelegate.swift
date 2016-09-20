2dc603f2c424b4639d352258fd37af553a2a33c8
Matthew Lewis <matt@mplewis.com>
2016-09-19 22:40:07 -0600

    app: show degrees to apple hq

--- Sternidae/AppDelegate.swift
+++ Sternidae/AppDelegate.swift
@@ -7,6 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     var window: UIWindow?
     var locMgr: CLLocationManager?
 
+    let appleHQ = Waypoint(
+        name: "Apple",
+        loc: CLLocation(latitude: 37.331843, longitude: -122.029574),
+        color: .orangeColor()
+    )
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         startListeningForLocation()
         return true
@@ -39,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
         print("New location: \(newLocation)")
+        print("Angle to Apple HQ: \(NavHelpers.haversine(from: newLocation, to: appleHQ.loc))")
     }
     
 }
