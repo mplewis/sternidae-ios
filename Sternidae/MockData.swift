a0195a38b3b96a72f2da11be1b7deeb8c9ea286c
Matthew Lewis <matt@mplewis.com>
2016-10-18 23:58:34 -0600

    mock: change location colors

--- Sternidae/MockData.swift
+++ Sternidae/MockData.swift
@@ -7,11 +7,11 @@ class MockData {
         Waypoint(
             name: "Home",
             loc: CLLocation(latitude: 39.729337, longitude: -104.927597),
-            color: UIColor(red:0.18, green:0.80, blue:0.44, alpha:1.0)),
+            color: UIColor(red:1.00, green:0.81, blue:0.35, alpha:1.0)),
         Waypoint(
             name: "Work",
             loc: CLLocation(latitude: 39.753386, longitude: -104.991516),
-            color: UIColor(red:0.20, green:0.60, blue:0.86, alpha:1.0)),
+            color: UIColor(red:0.39, green:0.84, blue:1.00, alpha:1.0)),
     ]
 
 }
